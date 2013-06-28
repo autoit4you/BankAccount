5525516fc53e6723008f7b372816c79bee73e383
autoit4you <gutenbergschule.horn@gmail.com>
2013-06-28 17:21:15 +0200

    Fixed a bug that the new subcommands didn't worked

--- src/main/java/me/autoit4you/bankaccount/BankAccount.java
+++ src/main/java/me/autoit4you/bankaccount/BankAccount.java
@@ -110,11 +110,11 @@ import org.mcstats.MetricsLite;
 	    			}else if(args[0].equalsIgnoreCase("removeuser")) {
 	    				new CommandAccountRemoveuser().run(sender, args);
 	    			}else if(args[0].equalsIgnoreCase("addadmin")) {
-	    				throw new BAArgumentException("That command is not recognized. Please type /account help for help.");
+	    				new CommandAccountRemoveuser().run(sender, args);
 	    			}else if(args[0].equalsIgnoreCase("removeadmin")) {
-	    				throw new BAArgumentException("That command is not recognized. Please type /account help for help.");
+	    				new CommandAccountRemoveuser().run(sender, args);
 	    			}else if(args[0].equalsIgnoreCase("transferownership")) {
-	    				throw new BAArgumentException("That command is not recognized. Please type /account help for help.");
+	    				new CommandAccountRemoveuser().run(sender, args);
 	    			}else{
 	    				throw new BAArgumentException("That command is not recognized. Please type /account help for help.");
 	    			}

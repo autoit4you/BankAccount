292a711d8f1d0f57764b87c4bfa0a96e0bec2f11
autoit4you <gutenbergschule.horn@gmail.com>
2013-08-04 22:17:49 +0200

    Added /account top, rewrote /account list uses now API, little change for the commandbase

--- src/main/java/me/autoit4you/bankaccount/commands/BankAccountCommand.java
+++ src/main/java/me/autoit4you/bankaccount/commands/BankAccountCommand.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import me.autoit4you.bankaccount.BankAccount;
 import me.autoit4you.bankaccount.exceptions.BAArgumentException;
-import me.autoit4you.bankaccount.exceptions.BankAccountException;
 
 import me.autoit4you.bankaccount.exceptions.CommandPermissionException;
 import org.bukkit.command.CommandSender;
 
 public abstract class BankAccountCommand {
 	
-	public abstract void run(CommandSender sender, String[] args, BankAccount plugin) throws BAArgumentException, CommandPermissionException;
+	public abstract void run(CommandSender sender, String[] args, BankAccount plugin)
+            throws BAArgumentException, CommandPermissionException;
 	
 	public abstract List<String> tab(CommandSender sender, String[] args);
 }
--- src/main/java/me/autoit4you/bankaccount/commands/CommandAccountList.java
+++ src/main/java/me/autoit4you/bankaccount/commands/CommandAccountList.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 import me.autoit4you.bankaccount.BankAccount;
+import me.autoit4you.bankaccount.api.Account;
 import me.autoit4you.bankaccount.exceptions.*;
 
 import org.bukkit.ChatColor;
@@ -12,37 +13,28 @@ import org.bukkit.command.CommandSender;
 public class CommandAccountList extends BankAccountCommand {
 
 	@Override
-	public void run(CommandSender sender, String[] args)
-			throws BankAccountException {
+	public void run(CommandSender sender, String[] args, BankAccount plugin)
+            throws BAArgumentException, CommandPermissionException {
 		if(!BankAccount.perm.user(sender, args))
 			throw new CommandPermissionException();
-		
-		String user = "";
-		String owner = "";
-		HashMap<String, String> accounts = BankAccount.db.getAccounts(sender.getName());
-		
-		for(Entry<String, String> account : accounts.entrySet()) {
-			if(account.getValue() == "1")
-				user += account.getKey() + ", ";
-			else if(account.getValue() == "2")
-				owner += account.getKey() + ", ";
-		}
-		
-		if(user.indexOf(",") != -1){
-			user = user.substring(0, user.length() - 2);
-		}
-		if(owner.indexOf(",") != -1){
-			owner = owner.substring(0, owner.length() - 2);
-		}
-		
-		if(owner == "" && user == "") {
-			sender.sendMessage(ChatColor.RED + "You don't have any accounts");
-		}else {
-			if(owner != "")
-				sender.sendMessage("Owner: " + owner);
-			if(user != "")
-				sender.sendMessage("User: " + user);
-		}
+
+        HashMap<String, Integer> access = new HashMap<String, Integer>();
+
+		for(Account acc : plugin.getAPI().getAccounts()) {
+            if(acc.getAccess(sender.getName()) != -1)
+                access.put(acc.getName(), acc.getAccess(sender.getName()));
+        }
+
+        for(int i=1; i<4; i++){
+            String names = "";
+            for(String account : access.keySet()) {
+                if(access.get(account) == i)
+                    names = names + account + ", ";
+            }
+            if(names != "")
+                sender.sendMessage(ChatColor.BLUE + plugin.getLanguageManager().getLocalString("command.list." + i) + ": " +
+                        names.substring(0, names.length() - 2));
+        }
 	}
 
 	@Override
